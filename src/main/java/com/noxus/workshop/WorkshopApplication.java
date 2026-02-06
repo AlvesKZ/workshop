ef24d193a5d7d8d1d44abd2b12f648c535d03588
AlvesKZ <igoralvex01@gmail.com>
2026-02-06 14:32:02 -0300

    create project

---
+++ src/main/java/com/noxus/workshop/WorkshopApplication.java
@@ -0,0 +1,13 @@
+package com.noxus.workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/noxus/workshop/WorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noxus.workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
